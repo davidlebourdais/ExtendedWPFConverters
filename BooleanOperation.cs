7755e9484c9a150f4bd0b114672feaffca5858ea
David Le Bourdais <dlb.devx@gmail.com>
2020-01-15 16:27:11 +0100

    [Dev] Added null or empty string to boolean converter

--- BooleanOperation.cs
+++ BooleanOperation.cs
@@ -44,4 +44,19 @@
         Xnor
     }
 
+    
+    /// <summary>
+    /// Just enumerates None and Not boolean operations.
+    /// </summary>
+    public enum ReducedBooleanOperation
+    {
+        /// <summary>
+        /// Do nothing operation
+        /// </summary>
+        None,
+        /// <summary>
+        /// Boolean inversion operation
+        /// </summary>
+        Not
+    }
 }
---
+++ NotNullOrEmptyStringToBooleanConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EMA.ExtendedWPFConverters
+{
+    /// <summary>
+    /// A converter that returns directly the opposite value of <see cref="string.IsNullOrEmpty(string)"/> on a passed string. 
+    /// A 'None' operation can be set to return the actual value of the method.
+    /// </summary>
+    public class NotNullOrEmptyStringToBooleanConverter : MarkupExtension, IValueConverter
+    {
+        /// <summary>
+        /// Specifies the operation to be applied with the converter.
+        /// </summary>
+        public ReducedBooleanOperation Operation { get; set; } = ReducedBooleanOperation.None;
+
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = string.IsNullOrEmpty(value as string);
+            return Operation == ReducedBooleanOperation.Not ? result : !result;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var is_not_null_or_empty = value is bool casted && casted;
+            var result = is_not_null_or_empty ? "not null nor empty" : null;
+            if (Operation == ReducedBooleanOperation.Not)
+                result = is_not_null_or_empty ? null : "not null nor empty";
+            return result;
+        }
+
+        /// <inheritdoc />
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
