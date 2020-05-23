6603878ff4c602c55caa06cefb68bde54018efda
David Le Bourdais <dlb.devx@gmail.com>
2020-05-24 00:50:08 +0200

    [Dev] Added NotNullConvertersTests

---
+++ ExtendedWPFConverters.Tests/NotNullConverters/NotNullConvertersTests.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using System.Windows;
+
+namespace EMA.ExtendedWPFConverters.Tests
+{
+    public class NotNullConvertersTests
+    {
+        #region NotNullToVisibilityConverter
+        [Theory]
+        [InlineData("not null", Visibility.Visible, Visibility.Collapsed)]
+        [InlineData("not null", Visibility.Visible, Visibility.Hidden)]
+        [InlineData("not null", Visibility.Collapsed, Visibility.Visible)]
+        [InlineData("not null", Visibility.Collapsed, Visibility.Hidden)]
+        [InlineData("not null", Visibility.Hidden, Visibility.Visible)]
+        [InlineData("not null", Visibility.Hidden, Visibility.Collapsed)]
+        [InlineData("not null", Visibility.Visible, Visibility.Visible)]
+        [InlineData(true, Visibility.Visible, Visibility.Collapsed)]
+        [InlineData(123, Visibility.Visible, Visibility.Collapsed)]
+        [InlineData(null, Visibility.Visible, Visibility.Collapsed)]
+        [InlineData(null, Visibility.Visible, Visibility.Hidden)]
+        [InlineData(null, Visibility.Collapsed, Visibility.Visible)]
+        [InlineData(null, Visibility.Collapsed, Visibility.Hidden)]
+        [InlineData(null, Visibility.Hidden, Visibility.Visible)]
+        [InlineData(null, Visibility.Hidden, Visibility.Collapsed)]
+        [InlineData(null, Visibility.Visible, Visibility.Visible)]
+        public void ConvertsNotNullToVisibility(object input, Visibility valueForNotNull, Visibility valueForNull)
+        {
+            var converter = new NotNullToVisibilityConverter() { ValueForNotNull = valueForNotNull, ValueForNull = valueForNull };
+            var result = converter.Convert(input, typeof(object), null, null);
+            if (input != null)
+                Assert.Equal(valueForNotNull, result);
+            else
+                Assert.Equal(valueForNull, result);
+        }
+        #endregion
+    }
+}
